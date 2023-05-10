f22ef6dfc416769fcaf8976cc17afcfe9b84c420
Default_User <udefault@default.com>
2023-05-10 17:10:27 -0300

    Update dos Arquivos da  Pasta Bankline

--- Bankline/main.js
+++ Bankline/main.js
@@ -1,4 +1,8 @@
-//alert('Olá Mundo');
+
+const nomeBeneficiario = document.getElementById('nome_beneficiario');
+let formValido = false;
+const form = document.getElementById('form_dep');
+
 
 function validaNome(nomeCompleto){
     const nomeComoArray = nomeCompleto.split(' ');
@@ -6,29 +10,39 @@ function validaNome(nomeCompleto){
     return nomeComoArray.length >= 2;
 }
 
-const form = document.getElementById('form_dep');
-
 form.addEventListener('submit', function(e){
-    
-    let formValido = false;
 
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome_beneficiario');
     const numeroConta = document.getElementById('numero_conta');
     const valorDeposito = document.getElementById('valor_deposito');
-    const menssagemSucesso = `Valor de: ${valorDeposito.value} foi depositado para o cliente ${nomeBeneficiario.value} - conta nº: ${numeroConta.value}`
+    const mensagemSucesso = `Valor de: <b>${valorDeposito.value}</b> foi depositado para o cliente <b>${nomeBeneficiario.value}</b> - conta nº: <b>${numeroConta.value}</b>`
 
     formValido = validaNome(nomeBeneficiario.value);
 
     if(formValido){
-        alert(menssagemSucesso);
+        const ContainerMensagemSucesso = document.querySelector('.mensagem_sucesso');
+        ContainerMensagemSucesso.innerHTML = mensagemSucesso;
+        ContainerMensagemSucesso.style.display = 'block';
+
         nomeBeneficiario.value = '';
         numeroConta.value = '';
         valorDeposito.value = '';
     }else{
-        alert('O nome não está completo!');
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.mensagem_erro').style.display = 'block';
     }
 })
 
+nomeBeneficiario.addEventListener('keyup', function(e){
+    console.log(e.target.value);
+    formValido = validaNome(e.target.value);
 
+    if(formValido){
+        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.mensagem_erro').style.display = 'none';
+    }else{
+        nomeBeneficiario.classList.add('error');
+        document.querySelector('.mensagem_erro').style.display = 'block';
+    }
+})
