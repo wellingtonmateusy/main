e0fbdcf15c1362805bd81ee50c2042c8dce92033
Default_User <udefault@default.com>
2023-06-05 17:20:56 -0300

    Update Calculadora_IMC 05/06

--- Calculadora_IMC/main.js
+++ Calculadora_IMC/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     
     $('#result').click(function(){
         buttons2 = $('.buttons2').css('display', 'none');
-        buttons = $('.buttons').css('display', 'grid');
+        buttons = $('.buttons').css('display', 'flex');
         $('.number').click(function() {
             var val = $(this).text();
             result.val(result.val() + val);
@@ -18,11 +18,14 @@ $(document).ready(function() {
         $('.pontuacao').click(function(){
             result.val(result.val() + '.');
         });
+        $('.calculate1').click(function(){
+            $('.mensagem').css('display', 'block')
+        });
     });
 
     $('#result2').click(function(){
         buttons = $('.buttons').css('display', 'none');
-        buttons2 = $('.buttons2').css('display', 'grid');
+        buttons2 = $('.buttons2').css('display', 'flex');
         $('.number2').click(function() {
             var val2 = $(this).text();
             result2.val(result2.val() + val2);
@@ -36,31 +39,30 @@ $(document).ready(function() {
         $('.pontuacao2').click(function(){
             result2.val(result2.val() + '.');
         });
+        $('.calculate2').click(function() {
+            var resultado = result.val() / (result2.val() * result2.val());
+        
+            if(resultado < 17){
+                resultado = $('.mensagem1').css('display', 'block');
+            }else if(resultado == 17 || resultado <= 18.49){
+                resultado = $('.mensagem2').css('display', 'block');
+            }else if(resultado == 18.5  || resultado <= 24.99){
+                resultado = $('.mensagem3').css('display', 'block');
+            }else if(resultado == 25 || resultado <= 29.99){
+                resultado = $('.mensagem4').css('display', 'block');
+            }else if(resultado == 30 || resultado <= 34.99){
+                resultado = $('.mensagem5').css('display', 'block');
+            }else if(resultado == 35 || resultado <= 39.99){
+                resultado = $('.mensagem6').css('display', 'block');
+            }else{
+                resultado = $('.mensagem7').css('display', 'block');
+            }
+        });
     });
 
     $('.clear_tudo').click(function(){
         result.val('');
         result2.val('');
         resultado = $('.mensagem1, .mensagem2, .mensagem3, .mensagem4, .mensagem5, .mensagem6, .mensagem7').css('display', 'none');
-    });
-
-    $('.calculate').click(function() {
-        var resultado = result.val() / (result2.val() * result2.val());
-
-        if(resultado < 17){
-            resultado = $('.mensagem1').css('display', 'block');
-        }else if(resultado == 17 || resultado <= 18.49){
-            resultado = $('.mensagem2').css('display', 'block');
-        }else if(resultado == 18.5  || resultado <= 24.99){
-            resultado = $('.mensagem3').css('display', 'block');
-        }else if(resultado == 25 || resultado <= 29.99){
-            resultado = $('.mensagem4').css('display', 'block');
-        }else if(resultado == 30 || resultado <= 34.99){
-            resultado = $('.mensagem5').css('display', 'block');
-        }else if(resultado == 35 || resultado <= 39.99){
-            resultado = $('.mensagem6').css('display', 'block');
-        }else{
-            resultado = $('.mensagem7').css('display', 'block');
-        }
     });
 });
